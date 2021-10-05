740266746b3595661b71506b0a494a2bbf32ec7a
Greg Williams <cleanpro@GregMacBook-Pro-2.local>
2021-10-05 17:15:17 -0400

    GUID test console

--- index.js
+++ index.js
@@ -79,6 +79,8 @@ express()
   ]);
   }
 
+  uuidv4();
+
   function uuidv4() {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
       // tslint:disable-next-line: no-bitwise
