70c24addbe5f59f2253cbbed712befed88c93823
Greg Williams <cleanpro@GregMacBook-Pro-2.local>
2021-10-05 17:14:01 -0400

    GUID test

--- index.js
+++ index.js
@@ -79,3 +79,17 @@ express()
   ]);
   }
 
+  function uuidv4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+      // tslint:disable-next-line: no-bitwise
+      const r =
+        (window.crypto.getRandomValues(new Uint32Array(1))[0] *
+          Math.pow(2, -32) * 16) |
+        0;
+      // tslint:disable-next-line: no-bitwise
+      const v = c === "x" ? r : (r & 0x3) | 0x8;
+      console.log("Guid: " + v.toString(16));
+      return v.toString(16);
+    });
+  }
+
