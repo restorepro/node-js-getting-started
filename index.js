a1563187f58ce41ea9e844f4c0b488270ddfd8bf
Greg Williams <cleanpro@GregMacBook-Pro-2.local>
2021-10-06 14:43:43 -0400

    new user updated test live

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cool = require('cool-ascii-faces');
 const express = require('express');
+//const cors = require("cors");
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 5000;
@@ -11,7 +12,7 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
-var bodyParser = require('body-parser')
+//var bodyParser = require('body-parser')
 var testUUID = uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 console.log("test UUID: " + testUUID);
 console.log(process.env.DATABASE_URL);
@@ -21,8 +22,9 @@ var count = todos.length;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use(express.urlencoded({extended: true}))
+  //.use(express.urlencoded({extended: true}))
   .use(express.json())
+  //.use(cors({origin: new URL('http://localhost:3000'), credentials: true}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
@@ -62,12 +64,15 @@ express()
     }
     ]);
   })
-.post('/newUser', (request, response) => {
-  var newTodo = JSON.parse(request.body);
-  count = count + 1;
-  newTodo.id = count;
-  todos.push(newTodo);
-  response.status(201).json();
+.get('/newuser', (request, response) => {
+  var user_name = request.body.my;
+  console.log("request body: " + request.body);
+  //var newTodo = JSON.parse(request.body);
+  var newTodo = request.body;
+   count = count + 1;
+   newTodo.id = count;
+   todos.push(newTodo);
+  response.status(201).json(request.body);
 })
   .get('/hello', (req, res) => res.send('Hello World'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
